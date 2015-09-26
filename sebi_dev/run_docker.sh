7ea36e24f4eca504f566283746ad33a14691f71f
sebi <gore.sebyx@yahoo.com>
2015-09-26 11:59:15 +0300

    lol

--- sebi_dev/run_docker.sh
+++ sebi_dev/run_docker.sh
@@ -2,5 +2,8 @@
 mkdir -p $HOME/db
 mkdir -p $HOME/db/postgres
 mkdir -p $HOME/db/mongo
+mkdir -p $HOME/docker
+mkdir -p $HOME/docker/ruby_gems
+mkdir -p $HOME/docker/ruby_gems/2.2.2
 
 docker-compose run sebi_dev /bin/bash
